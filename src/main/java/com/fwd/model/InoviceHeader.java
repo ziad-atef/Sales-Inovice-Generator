41fb51c7a190619c72a8e0f90bfd265720e30322
ziad729 <ziadatef35@hotmail.com>
2022-07-29 17:30:42 +0200

    Initial commit

---
+++ src/main/java/com/fwd/model/InoviceHeader.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.fwd.model;
+import java.util.Date;
+/**
+ *
+ * @author ziada
+ */
+public class InoviceHeader {
+    int InoviceNumber;
+    Date InoviceDate;
+    String CustomerName;
+    InoviceLine InoviceLines;
+}
